9474b92ab59fa3110d09d7e3b858abbdb6989e6b
josephlara <joseph.lara@gmail.com>
2023-06-22 21:56:25 +0200

    * develop demproj ingestion script

--- Scripts/tidy_excel_proj.R
+++ Scripts/tidy_excel_proj.R
@@ -1,50 +1,14 @@
+
 library(tidyverse)
 library(janitor)
 library(openxlsx)
 library(readxl)
 library(fs)
+library(googlesheets4)
+library(glamr)
+load_secrets()
 
 # FUNCTION ----------------------------------------------------------------
-
-# 
-# extract_demproj <- function(filename, tab) {
-#   
-#   df <- read_excel(path = filename,
-#                    sheet = tab,
-#                    skip = 87,
-#                    col_names = FALSE) %>% 
-#     rename(
-#       age = `...1`,
-#       total_total = `...2`,
-#       total_male = `...3`,
-#       total_female = `...4`,
-#       urban_total = `...5`,
-#       urban_male = `...6`,
-#       urban_female = `...7`,
-#       rural_total = `...8`,
-#       rural_male = `...9`,
-#       rural_female = `...10`
-#     ) %>%
-#     mutate(district = case_when(str_detect(age, pattern = "idade. ") ~ age,
-#                                 TRUE ~ NA_character_)) %>% 
-#     fill(district, .direction = "down") %>% 
-#     filter(!str_detect(age, pattern = "Idade|Total|Quadro")) %>% 
-#     mutate(district = str_extract(district, "(?<=idade\\. ).*")) %>% 
-#     mutate(year = str_extract(district, "(?<=,).*")) %>% 
-#     mutate(district = str_extract(district, "^.*(?=,)"))
-#   
-#   year_var <- str_trim(df$year[1])
-#   
-#   df <- df %>% 
-#     mutate(year = year_var) %>% 
-#     relocate(any_of(c("district", "year")), .before = everything())
-#   
-#   return(df)
-#   
-# }
-
-
-path_df <- "Data/nampula.xlsx"
 
 extract_demproj <- function(filename, tab) {
   
@@ -77,52 +41,81 @@ extract_demproj <- function(filename, tab) {
   
 }
 
-df <- extract_demproj(path_df, "2022")
+# PATHS & LISTS -------------------------------------------------------------------
 
+path_nampula <- "Data/nampula.xlsx"
+path_sofala <- "Data/sofala.xlsx"
+path_manica <- "Data/manica.xlsx"
+path_tete <- "Data/tete.xlsx"
+path_niassa <- "Data/niassa.xlsx"
+path_gaza <- "Data/gaza.xlsx"
+path_zambezia <- "Data/zambezia.xlsx"
+path_cabo_delgado <- "Data/cabo_delgado.xlsx"
+path_inhambane <- "Data/inhambane.xlsx"
+path_maputo_cidade <- "Data/maputo_cidade.xlsx"
+path_maputo_province <- "Data/maputo_province.xlsx"
 
-excel_tabs <- excel_sheets(path_df)
+excel_tabs <- as.character(c(2017:2050))
 
-df_combine <- map_dfr(.x = c(excel_tabs),
-                      .f = ~ extract_demproj(path_df, .x))
-
-unique(df_combine$year)
-
-# PATHS -------------------------------------------------------------------
-
-
-# paths & values
-path_data_repo <- "Data/" # path to repo where original files are stored
-path_dataout <- "Dataout/" # path to repo where corrected files will be saved
-
-path_demproj_repo <- dir({path_data_repo}, pattern = "*.xlsx")
-
-tpt_historic <- path_demproj_repo %>%
-  map(~ read_tsv(file.path(path_monthly_output_repo, .))) %>%
-  reduce(rbind) 
-
-# batch load files to be processed
-batch_list <- path_data_repo %>% 
-  dir_ls() %>% 
-  map(
-    .f = function(path){
-      read_tsv(
-        path
-      )
-    }
-  )
+map_psnu <- read_sheet(as_sheets_id("13mufLaIKsTdgBx5FKPuoHeqt4UMrfx4eZgaEjDW9qJQ"), sheet = "ine_demproj_psnu")
 
 # PROCESSING --------------------------------------------------------------
 
-path_df_nam <- "Data/nampula.xlsx"
+df_nampula <- map_dfr(.x = c(excel_tabs),
+                      .f = ~ extract_demproj(path_nampula, .x))
 
-df <- extract_demproj(filename = path_df_nam, tab = "2021")
-unique(df$year)
+df_sofala <- map_dfr(.x = c(excel_tabs),
+                     .f = ~ extract_demproj(path_sofala, .x))
 
-excel_tabs <- excel_sheets(path_df_nam)
+df_manica <- map_dfr(.x = c(excel_tabs),
+                     .f = ~ extract_demproj(path_manica, .x))
 
-df_combine <- map_dfr(.x = c(excel_tabs),
-                      .f = ~ extract_demproj(path_df_nam, .x))
+df_tete <- map_dfr(.x = c(excel_tabs),
+                   .f = ~ extract_demproj(path_tete, .x))
 
+df_niassa <- map_dfr(.x = c(excel_tabs),
+                     .f = ~ extract_demproj(path_niassa, .x))
 
-unique(df_combine$year)
+df_cabo_delgado <- map_dfr(.x = c(excel_tabs),
+                           .f = ~ extract_demproj(path_cabo_delgado, .x))
 
+df_zambezia <- map_dfr(.x = c(excel_tabs),
+                       .f = ~ extract_demproj(path_zambezia, .x))
+
+df_inhambane <- map_dfr(.x = c(excel_tabs),
+                        .f = ~ extract_demproj(path_inhambane, .x))
+
+df_gaza <- map_dfr(.x = c(excel_tabs),
+                   .f = ~ extract_demproj(path_gaza, .x))
+
+df_maputo_province <- map_dfr(.x = c(excel_tabs),
+                              .f = ~ extract_demproj(path_maputo_province, .x))
+
+df_maputo_cidade <- map_dfr(.x = c(excel_tabs),
+                            .f = ~ extract_demproj(path_maputo_cidade, .x))
+
+# COMPILE -----------------------------------------------------------------
+
+df_compile <- bind_rows(df_cabo_delgado, 
+                        df_gaza, 
+                        df_inhambane, 
+                        df_manica, 
+                        df_maputo_cidade,
+                        df_maputo_province,
+                        df_nampula,
+                        df_niassa,
+                        df_sofala,
+                        df_tete,
+                        df_zambezia) %>% 
+  pivot_longer(cols = total_total:rural_female, names_to = c("urban_rural", "sex"), names_sep = "_", values_to = "value") %>% 
+  filter(!urban_rural == "total",
+         !sex == "total") %>% 
+  left_join(map_psnu, by = "district") %>% 
+  relocate(any_of(c("snu", "psnu", "snuuid", "psnuuid")), .before = everything()) %>% 
+  select(!district)
+
+# WRITE TO DISK -----------------------------------------------------------
+
+write_csv(df_compile,
+          "Dataout/ine_demproj.csv",
+          na = "")
